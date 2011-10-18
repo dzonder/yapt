bcae4d65a7d3492c020b71608b3d58c11ca41edb
Michal Olech <michal.olech@students.mimuw.edu.pl>
2011-10-18 23:20:30 +0200

    Added templates

--- display.php
+++ display.php
@@ -8,12 +8,11 @@
  */
 
 include_once 'libs/config.inc.php';
+include_once 'libs/rain.tpl.class.php';
 
-function _die($msg=false) {
-    if ($msg) echo $msg;
-    mysql_close();
-    exit;
-}
+$tpl = new RainTPL();
+
+$tpl->assign('subtitle', 'Display encrypted paste');
 
 if (!isset($_GET['id'])) {
     die('ID undefined!');
@@ -26,8 +25,10 @@ mysql_select_db(MYSQL_DATABASE);
 $result = mysql_query("SELECT id, flags, raw_code, highlighted_code FROM pastes WHERE id=" .hexdec($_GET['id']));
 $paste = mysql_fetch_assoc($result);
 
+mysql_close();
+
 if (!isset($paste['id'])) {
-    _die('ID does not exist!');
+    die('ID does not exist!');
 }
 
 if (isset($_GET['raw'])) {
@@ -38,33 +39,8 @@ if (isset($_GET['raw'])) {
 
 if (strpos($paste['flags'], 'ENC_3DES') !== false) {
     if (!isset($_POST['passwd'])) {
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <title><?php echo CONF_TITLE; ?> - Display encrypted paste</title>
-    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
-  </head>
-  <body>
-    <h1><?php echo CONF_TITLE; ?></h1>
-    <h2>Display encrypted paste</h2>
-    <p>This paste is encrypted. Enter password to view the code. If you enter wrong password you will see thrash.</p>
-    <form method="post" action="<?php echo $_GET['id']; ?>">
-      <div>
-        <label for="frm-passwd">Password</label>
-        <input type="password" name="passwd" id="frm-passwd" />
-      </div>
-      <div>
-        <input type="submit" value="Submit" />
-      </div>
-    </form>
-    <p>Copyright &copy; 2011 by <a href="mailto:<?php echo CONF_FOOTER_EMAIL; ?>"><?php echo CONF_FOOTER_NAME; ?></a></p>
-    <p><a href="http://validator.w3.org/check?uri=referer"><img src="valid-xhtml10.png" alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a></p>
-  </body>
-</html>
-<?php
-        _die();
+        $tpl->draw('password');
+        die();
     } else {
         $hash = mhash(MHASH_SHA1, $_POST['passwd']);
         $code = mcrypt_decrypt(MCRYPT_3DES, $hash, $code, MCRYPT_MODE_ECB);
@@ -74,21 +50,8 @@ if (strpos($paste['flags'], 'ENC_3DES') !== false) {
 if (isset($_GET['raw'])) {
     echo $code;
 } else {
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <title><?php echo CONF_TITLE; ?> - Paste ID <?php echo dechex($paste['id']); ?></title>
-    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
-  </head>
-  <body>
-    <?php echo $code; ?>
-  </body>
-</html>
-<?php
+    $tpl->assign('code', $code);
+    $tpl->draw('display');
 }
-
-mysql_close();
 
 ?>
--- index.php
+++ index.php
@@ -8,43 +8,12 @@
  */
 
 include_once 'libs/config.inc.php';
+include_once 'libs/rain.tpl.class.php';
+
+$tpl = new RainTPL();
+
+$tpl->assign('subtitle', 'Add new paste');
+$tpl->assign('languages', $languages);
+$tpl->draw('index');
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <title><?php echo CONF_TITLE; ?> - Add new paste</title>
-    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
-  </head>
-  <body>
-    <h1><?php echo CONF_TITLE; ?></h1>
-    <h2>Add new paste</h2>
-    <form method="post" action="paste">
-      <div>
-        <label for="frm-passwd">Password</label>
-        <input type="password" name="passwd" id="frm-passwd" />
-        <p class="description">If supplied, pasted code will be encrypted in database, otherwise everyone can view the paste.</p>
-      </div>
-      <div>
-        <label for="frm-lang">Highlight</label>
-        <select name="lang" id="frm-lang">
-          <option value="none">None</option>
-<?php
-    foreach ($languages as $lang) {
-        echo '<option value="' .$lang .'">' .ucfirst($lang) .'</option>';
-    }
-?>
-        </select>
-      </div>
-      <div>
-        <textarea name="code" rows="20" cols="85"></textarea>
-      </div>
-      <div>
-        <input type="submit" value="Submit" />
-      </div>
-    </form>
-    <p>Copyright &copy; 2011 by <a href="mailto:<?php echo CONF_FOOTER_EMAIL; ?>"><?php echo CONF_FOOTER_NAME; ?></a></p>
-    <p><a href="http://validator.w3.org/check?uri=referer"><img src="valid-xhtml10.png" alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a></p>
-  </body>
-</html>
