dd269e9a7114b048aed34a341e66682a5994dbef
Michal Olech <michal.olech@students.mimuw.edu.pl>
2011-10-19 01:56:56 +0200

    Added table sql schema

---
+++ create_tables.sql
@@ -0,0 +1,11 @@
+-- Copyright (c) 2011 Michal Olech
+-- YAPT
+
+CREATE TABLE `pastes` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `timesent` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `flags` set('ENC_3DES','ENC_PLAIN') NOT NULL,
+  `language` varchar(10) NOT NULL DEFAULT 'none',
+  `code` blob NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=128 DEFAULT CHARSET=utf8
